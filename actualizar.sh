193e3c4e6388590bad0b135b67e84a3c46a690e9
hackingyseguridad <antonio_taboada@hotmail.es>
2022-06-23 12:01:09 +0200

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -5,6 +5,7 @@ chmod 777 *
 wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/diccionario.txt -q -O diccionario.txt
 wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/usuarios.txt -q -O usuarios.txt
 wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/claves.txt -q -O claves.txt
+wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/claves2.txt -q -O claves2.txt
 echo "."
 echo ".."
 echo "... fin!"
