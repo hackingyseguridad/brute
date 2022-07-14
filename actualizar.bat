9a3bd777f4ca803ebde145bd54f69ea57049c5f0
hackingyseguridad <antonio_taboada@hotmail.es>
2022-07-14 10:25:26 +0200

    Update actualizar.bat

--- actualizar.bat
+++ actualizar.bat
@@ -2,16 +2,16 @@
 echo " "
 echo "... actualizando diccionarios ...   2022 hackingyseguridad.com "
 echo ".."
-curl -k https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/diccionario.txt -q -O diccionario.txt
+curl -k https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/diccionario.txt -O 
 echo "diccionario.txt"
 echo ".."
-curl -k https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/usuarios.txt -q -O usuarios.txt
+curl -k https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/usuarios.txt -O
 echo "usuarios.txt"
 echo ".."
-curl -k https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/claves.txt -q -O claves.txt
+curl -k https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/claves.txt -O
 echo "claves.txt"
 echo ".."
-curl -k https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/claves2.txt -q -O claves2.txt
+curl -k https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/claves2.txt -O
 echo "claves2.txt"
 echo ".."
 echo "..."
