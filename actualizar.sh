66838d277bca9dcaafa7b150ea8e389b9500b9c1
hackingyseguridad <antonio_taboada@hotmail.es>
2022-09-12 12:23:36 +0200

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -18,6 +18,9 @@ echo ".."
 wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/claves2.txt -q -O claves2.txt
 echo "claves2.txt"
 echo ".."
+wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/claves0.txt -q -O claves0.txt
+echo "claves0.txt"
+echo ".."
 echo "..."
 echo "...."
 echo "..... fin!"
