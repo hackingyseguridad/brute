7de32dcd39fe13025fc57d36cc14e120b975d3ef
hackingyseguridad <antonio_taboada@hotmail.es>
2023-11-13 13:54:32 +0100

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -3,22 +3,22 @@ echo
 echo "... actualizando diccionarios ...   2022 hackingyseguridad.com "
 chmod 777 *
 echo ".."
-wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/diccionario.txt -q -O diccionario.txt
+wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/diccionario.txt -q -O diccionario.txt  --inet4-only
 echo "diccionario.txt"
 echo ".."
-wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/usuarios.txt -q -O usuarios.txt
+wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/usuarios.txt -q -O usuarios.txt  --inet4-only
 echo "usuarios.txt"
 echo ".."
-wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/usuarios0.txt -q -O usuarios0.txt
+wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/usuarios0.txt -q -O usuarios0.txt  --inet4-only
 echo "usuarios0.txt"
 echo ".."
-wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/claves.txt -q -O claves.txt
+wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/claves.txt -q -O claves.txt  --inet4-only
 echo "claves.txt"
 echo ".."
-wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/claves2.txt -q -O claves2.txt
+wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/claves2.txt -q -O claves2.txt  --inet4-only
 echo "claves2.txt"
 echo ".."
-wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/claves0.txt -q -O claves0.txt
+wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/master/claves0.txt -q -O claves0.txt  --inet4-only
 echo "claves0.txt"
 echo ".."
 echo "..."
