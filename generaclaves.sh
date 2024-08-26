a4e6fc9d66c4a90a6dc4e3ad5b1518b927db5b04
hackingyseguridad <antonio_taboada@hotmail.es>
2024-08-26 17:02:13 +0200

    Update generaclaves.sh

--- generaclaves.sh
+++ generaclaves.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # (c) Hacking y Seguridad .com 2024
 # Generacdor de claves ccombinando dos ficheros
+# > passw.txt para guardar en un fichero el resultado
 
 crunch 2 3 -f /usr/share/crunch/charset.lst mixalpha-numeric -o combinaciones.txt
 
