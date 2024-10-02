f3898e9b044babee23b38d331d40ac215d9b4b24
hackingyseguridad <antonio_taboada@hotmail.es>
2024-10-02 21:15:20 +0200

    Update brutepop3b.sh

--- brutepop3b.sh
+++ brutepop3b.sh
@@ -1,4 +1,47 @@
+#!/bin/bash
+cat << "INFO"
 
+                                                          ..
+                               ,,,                         MM .M
+                           ,!MMMMMMM!,                     MM MM  ,.
+   ., .M                .MMMMMMMMMMMMMMMM.,          'MM.  MM MM .M'
+ . M: M;  M          .MMMMMMMMMMMMMMMMMMMMMM,          'MM,:M M'!M'
+;M MM M: .M        .MMMMMMMMMMMMMMMMMMMMMMMMMM,         'MM'...'M
+ M;MM;M :MM      .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.       .MMMMMMMM
+ 'M;M'M MM      MMMMMM  MMMMMMMMMMMMMMMMM  MMMMMM.    ,,M.M.'MMM'
+  MM'MMMM      MMMMMM @@ MMMMMMMMMMMMMMM @@ MMMMMMM.'M''MMMM;MM'
+ MM., ,MM     MMMMMMMM  MMMMMMMMMMMMMMMMM  MMMMMMMMM      '.MMM
+ 'MM;MMMMMMMM.MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.      'MMM
+  ''.'MMM'  .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM       MMMM
+   MMC      MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.      'MMMM
+  .MM      :MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM''MMM       MMMMM
+  MMM      :M  'MMMMMMMMM brute pop3 MMMMMMM'.MM  MM:M.    'MMMMM
+ .MMM   ...:M: :M.'MMMMMMMMMMMMMMMMMMMMMMMMM'.M''   MM:MMMMMMMMMMMM'
+AMMM..MMMMM:M.    :M.'hackingyseguridad.com'.MM'     MM''''''''''''
+MMMMMMMMMMM:MM     'M'.M'MMMMMMMMMMMMMM'.MC'M'     .MM
+ '''''''''':MM.       'MM!M.'M-M-M-M'M.'MM'        MMM
+            MMM.            'MMMM!MMMM'            .MM
+             MMM.             '''   ''            .MM'
+              MMM.                               MMM'
+               MMMM            ,.J.JJJJ.       .MMM'
+                MMMM.       'JJJJJJJ'JJJM   CMMMMM
+                  MMMMM.    'JJJJJJJJ'JJJ .MMMMM'
+                    MMMMMMMM.'  'JJJJJ'JJMMMMM'
+                      'MMMMMMMMM'JJJJJ JJJJJ'
+                         ''MMMMMMJJJJJJJJJJ'
+                                 'JJJJJJJJ'
+
+INFO
+if [ -z "$1" ]; then
+        echo
+        echo "Fuerza bruta con diccionarios a pop3 en puerto por defecto 110/TCP."
+        echo "Requiere nmap"
+        echo "Uso.: sh brutepop3b.sh <ip>"
+        echo
+        exit 0
+fi
+echo
+echo
 
 # hydra -L usuarios0.txt -P claves0.txt -f $1 $2 $3 pop3 -V
 
