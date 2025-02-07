8bcbc3e14d00d38305026430a71cf9683cfcfe33
hackingyseguridad <antonio_taboada@hotmail.es>
2025-02-07 15:55:56 +0100

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -4,5 +4,6 @@ apt-get install ncrack
 apt-get install hydra
 apt-get install medusa
 apt-get install nmap
+apt-get install sshpass
 curl -sSfL 'https://git.io/kitabisa-ssb' | sh -s -- -b /usr/local/bin
 sh actualizar.sh
