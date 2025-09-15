506fb1cfea8049ab1814414c0285d4aec833a067
hackingyseguridad <antonio_taboada@hotmail.es>
2025-09-15 13:01:18 +0200

    Create brutesip.sh

---
+++ brutesip.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+cat << "INFO"
+                                                          ..
+                               ,,,                         MM .M
+                           ,!MMMMMMM!,                     MM MM  ,.
+   ., .M                .MMMMMMMMMMMMMMMM.,          'MM.  MM MM .M'
+ . M: M;  M          .MMMMMMMMMMMMMMMMMMMMMM,          'MM,:M M'!M'
+;M MM M: .M        .MMMMMMMMMMMMMMMMMMMMMMMMMM,         'MM'...'M
+ M;MM;M :MM      .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.       .MMMMMMMM
+ 'M;M'M MM      MMMMMM  MMMMMMMMMMMMMMMMM  MMMMMM.    ,,M.M.'MMM'
+  MM'MMMM      MMMMMM @@ MMMMMMMMMMMMMMM @@ MMMMMMM.'M''MMMM;MM'
+ MM., ,MM     MMMMMMMM  MMMMMMMMMMMMMMMMM  MMMMMMMMM      '.MMM
+ 'MM;MMMMMMMM.MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.      'MMM
+  ''.'MMM'  .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM       MMMM
+   MMC      MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.      'MMMM
+  .MM      :MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM''MMM       MMMMM
+  MMM      :M  'MMMMMMMMMMM Brute SIP MMMMMMMM'.MM  MM:M.    'MMMMM
+ .MMM   ...:M: :M.'MMMMMMMMMMMMMMMMMMMMMMMMM'.M''   MM:MMMMMMMMMMMM'
+AMMM..MMMMM:M.    :M.'MMMMMMMMMMMMMMMMMMMM'.MM'     MM''''''''''''
+MMMMMMMMMMM:MM     'M'.M'MMMMMMMMMMMMMM'.MC'M'     .MM
+ '''''''''':MM.       'MM!M.'M-M-M-M'M.'MM'        MMM
+            MMM.            'MMMM!MMMM'            .MM
+             MMM.             '''   ''            .MM'
+              MMM.                               MMM'
+               MMMM            ,.J.JJJJ.       .MMM'
+                MMMM.       'JJJJJJJ'JJJM   CMMMMM
+                  MMMMM.    'JJJJJJJJ'JJJ .MMMMM'
+                    MMMMMMMM.'  'JJJJJ'JJMMMMM'
+                      'MMMMMMMMM'JJJJJ JJJJJ'
+                         ''MMMMMMJJJJJJJJJJ'
+                                 'JJJJJJJJ'
+INFO
+if [ -z "$1" ]; then
+        echo
+        echo "Fuerza bruta con diccionarios a SIP."
+        echo "Requiere nmap"
+        echo "Uso.: sh brutesip.sh <ip>"
+        echo
+        exit 0
+fi
+echo
+echo
+nmap -p5060,5061 --script sip-brute --script-args userdb=usuarios0.txt,passdb=claves0.txt $1 -vvvv
