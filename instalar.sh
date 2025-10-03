ab9877796bdd32d7a23bb6823f4134ccbe90a628
hackingyseguridad <antonio_taboada@hotmail.es>
2025-10-03 11:39:33 +0200

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -5,5 +5,7 @@ apt-get install hydra
 apt-get install medusa
 apt-get install nmap
 apt-get install sshpass
+apt-get install crowbar
+apt-get install freerdp2-x11
 curl -sSfL 'https://git.io/kitabisa-ssb' | sh -s -- -b /usr/local/bin
 sh actualizar.sh
